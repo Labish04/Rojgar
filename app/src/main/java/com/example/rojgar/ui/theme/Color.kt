ae86c5d06c7de9ef9fd7168242470945ffe41b99
bhawanabhatt-web <bbhawana131@gmail.com>
2025-11-28 13:45:21 +0545

    JobSeeker profile created.

--- app/src/main/java/com/example/rojgar/ui/theme/Color.kt
+++ app/src/main/java/com/example/rojgar/ui/theme/Color.kt
@@ -3,6 +3,9 @@ package com.example.rojgar.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val Blue = Color(0xFFE4EEFC)
+val DarkBlue = Color(0xFFC3D5EE)
+val SkyBlue = Color(0xFF80C9F7)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
