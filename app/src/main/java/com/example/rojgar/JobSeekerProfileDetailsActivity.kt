ed20ee0f90e5e40211c49bec24ffce7edd155307
bhawanabhatt-web <bbhawana131@gmail.com>
2025-11-29 23:47:15 +0545

    JobSeeker Profile Details navigation done.

--- app/src/main/java/com/example/rojgar/JobSeekerProfileDetailsActivity.kt
+++ app/src/main/java/com/example/rojgar/JobSeekerProfileDetailsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.rojgar
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,6 +28,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -56,6 +60,7 @@ class JobSeekerProfileDetailsActivity : ComponentActivity() {
 fun JobSeekerProfileDetailsBody() {
 
     val context = LocalContext.current
+    val activity = context as Activity
 
     Scaffold { padding ->
         Column(
@@ -159,14 +164,22 @@ fun JobSeekerProfileDetailsBody() {
                 Card(
                     shape = RoundedCornerShape(10.dp),
                     modifier = Modifier.height(60.dp)
-                        .width(400.dp),
+                        .width(400.dp)
+                        .clickable(interactionSource = remember {
+                            MutableInteractionSource()
+                        },
+                        indication = null    ){
+                            val intent = Intent(context, JobSeekerPersonalInformationActivity ::class.java)
+                            context.startActivity(intent)
+                        },
                     colors = CardDefaults.cardColors(
                         containerColor = White
                     )
 
                 )
-
                 {
+
+
                     Row(
                         modifier = Modifier.fillMaxWidth()
                             .padding(vertical = 8.dp),
