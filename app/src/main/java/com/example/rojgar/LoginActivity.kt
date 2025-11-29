ae59972f31ac198f85388b58e3cd10c59dacf545
Labish04 <labishparajuli04@gmail.com>
2025-11-29 18:48:49 +0545

    Login fronted completed

---
+++ app/src/main/java/com/example/rojgar/LoginActivity.kt
@@ -0,0 +1,317 @@
+package com.example.rojgar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.rojgar.ui.theme.Blue
+import com.example.rojgar.ui.theme.Purple
+import com.example.rojgar.ui.theme.RojgarTheme
+import com.example.rojgar.ui.theme.White
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginBody()
+
+        }
+    }
+}
+
+@Composable
+fun LoginBody() {
+
+    var email by remember { mutableStateOf("") }
+
+    Scaffold { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding()
+                .background(color = Color.White)
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(R.drawable.design1),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .offset(x = -80.dp, y = -80.dp)
+                        .rotate(10f)
+                        .size(250.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.mandesign1),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .offset(y = 150.dp)
+                            .size(200.dp)
+                    )
+                }
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 90.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Column {
+                    Text("Welcome to Rojgar",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+
+                    Text("Find your dream job or hire top talent.",
+                        style = TextStyle(
+                            fontSize = 12.sp
+                        ),
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                    )
+                }
+            }
+            
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text("Don't hanve an account?",
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    ))
+                Spacer(modifier = Modifier.width(5.dp))
+                Text("SignUp",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        color = Purple
+                    ))
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+                OutlinedTextField(
+                    value = email,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
+                    onValueChange = { data ->
+                        email = data
+                    },
+
+                    placeholder = {
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically
+                        ){
+                            Image(
+                                painter = painterResource(R.drawable.outline_email_24),
+                                contentDescription = null,
+                                modifier = Modifier
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text("Email")
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp),
+                    shape = RoundedCornerShape(15.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = White,
+                        unfocusedContainerColor = White,
+                        focusedIndicatorColor = Blue,
+                        unfocusedIndicatorColor = Blue
+                    )
+                )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+                OutlinedTextField(
+                    value = email,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    ),
+                    onValueChange = { data ->
+                        email = data
+                    },
+                    placeholder = {
+                        Row {
+                            Image(
+                                painter = painterResource(R.drawable.outline_lock_24),
+                                contentDescription = null
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text("Password")
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp, vertical = 10.dp),
+                    shape = RoundedCornerShape(15.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = White,
+                        unfocusedContainerColor = White,
+                        focusedIndicatorColor = Blue,
+                        unfocusedIndicatorColor = Blue
+                    )
+                )
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(end = 30.dp),
+                horizontalArrangement = Arrangement.End
+            ){
+                Text(
+                    "Forget Password?", style = TextStyle(
+                        fontSize = 15.sp,
+                        color = Purple
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Purple
+                    ),
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    Text("Login", style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    ))
+                }
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 20.dp, horizontal = 30.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f)
+                )
+                Text("OR", modifier = Modifier.padding(horizontal = 15.dp))
+                HorizontalDivider(
+                    modifier = Modifier.weight(1f)
+                )
+            }
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Purple
+                    ),
+                    shape = RoundedCornerShape(12.dp),
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Image(
+                            painter = painterResource(R.drawable.google),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(40.dp)
+                                .padding(horizontal = 10.dp)
+                        )
+                        Text(
+                            "Login with Google", style = TextStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp
+                            )
+                        )
+                    }
+                }
+            }
+            Image(
+                painter = painterResource(R.drawable.design2),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(300.dp)
+                    .offset(x = 200.dp, y = 60.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GreetingPreview2() {
+    LoginBody()
+}
--- app/src/main/java/com/example/rojgar/ui/theme/Color.kt
+++ app/src/main/java/com/example/rojgar/ui/theme/Color.kt
@@ -3,6 +3,9 @@ package com.example.rojgar.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
+val White = Color(0xFFFFFFFF)
+val Blue = Color(0xFF0028C0)
+val Purple = Color(0xFF7253F8)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
