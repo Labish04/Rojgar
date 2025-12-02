bf66adb7d8b15fa3973920cd9757b541c600219f
bhawanabhatt-web <bbhawana131@gmail.com>
2025-12-02 21:06:31 +0545

    JobSeeker Experience add Button added.

--- app/src/main/java/com/example/rojgar/JobSeekerExperienceActivity.kt
+++ app/src/main/java/com/example/rojgar/JobSeekerExperienceActivity.kt
@@ -17,9 +17,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -31,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import com.example.rojgar.ui.theme.Blue
 import com.example.rojgar.ui.theme.DarkBlue2
 import com.example.rojgar.ui.theme.RojgarTheme
+import com.example.rojgar.ui.theme.SkyBlue
 
 class JobSeekerExperienceActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,7 +112,49 @@ fun JobSeekerExperienceBody() {
                     color = Color.Black
                 )
 
-                Spacer(modifier = Modifier.height(200.dp))
+                Spacer(modifier = Modifier.height(650.dp))
+                Row (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 20.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+
+                    Button (
+                        onClick = {
+
+                        },
+                        shape = RoundedCornerShape(25.dp),
+                        modifier = Modifier
+                            .width(150.dp)
+                            .height(45.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = DarkBlue2,
+                            contentColor = Color.White
+                        )
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.addexperience),
+                                contentDescription = "Add",
+                                modifier = Modifier.size(27.dp)
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = "Add",
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                ),
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }
+                    }
+                }
+
 
             }
         }
