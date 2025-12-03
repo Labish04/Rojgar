a4522bb09a1592ba1ae929cc57dfcb672e8b8575
bhawanabhatt-web <bbhawana131@gmail.com>
2025-12-03 19:17:32 +0545

    JobSeeker Skill card added.

--- app/src/main/java/com/example/rojgar/JobSeekerProfileDetailsActivity.kt
+++ app/src/main/java/com/example/rojgar/JobSeekerProfileDetailsActivity.kt
@@ -294,7 +294,6 @@ fun JobSeekerProfileDetailsBody() {
 
                 }
             }
-
             Spacer(modifier = Modifier.height(20.dp))
 
             Row (
@@ -349,6 +348,67 @@ fun JobSeekerProfileDetailsBody() {
                             modifier = Modifier
                                 .size(350.dp)
                                 .padding(end = 0.dp)
+                        )
+                    }
+
+
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement =Arrangement.Center
+            ){
+
+                Card(
+                    shape = RoundedCornerShape(10.dp),
+                    modifier = Modifier.height(60.dp)
+                        .width(400.dp)
+                        .clickable(interactionSource = remember {
+                            MutableInteractionSource()
+                        },
+                            indication = null    ){
+                            val intent = Intent(context, JobSeekerExperienceActivity::class.java)
+                            context.startActivity(intent)
+                        },
+
+                    colors = CardDefaults.cardColors(
+                        containerColor = White
+                    )
+                )
+                {
+                    Row(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.skillicon),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(40.dp)
+                                .padding(start =10.dp)
+                        )
+                        Text("Skill",
+                            style = TextStyle(
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.Normal,
+                            ),
+                            modifier = Modifier.padding(start = 10.dp)
+
+                        )
+                        Spacer(modifier = Modifier.width(175.dp))
+
+                        Icon(
+                            painter = painterResource(R.drawable.outline_keyboard_arrow_right_24),
+                            contentDescription = null,
+                            tint = Color.Black,
+                            modifier = Modifier
+                                .size(350.dp)
+                                .padding(start = 67.dp)
                         )
                     }
 
