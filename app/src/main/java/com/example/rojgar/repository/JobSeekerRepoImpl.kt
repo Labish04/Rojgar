e9181b463c8f7e1427b688cca9995da1d5cbc4ee
Labish04 <labishparajuli04@gmail.com>
2025-12-11 20:07:24 +0545

    JobSeeker Repo Implementation

--- app/src/main/java/com/example/rojgar/repository/JobSeekerRepoImpl.kt
+++ app/src/main/java/com/example/rojgar/repository/JobSeekerRepoImpl.kt
@@ -1,10 +1,14 @@
 package com.example.rojgar.repository
 
+import androidx.compose.runtime.mutableStateListOf
 import com.example.rojgar.model.JobSeekerModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class JobSeekerRepoImpl : JobSeekerRepo {
 
@@ -59,37 +63,80 @@ class JobSeekerRepoImpl : JobSeekerRepo {
                 callback(true,"Registration Successful")
             }
             else{
-                callback(false, "$")
+                callback(false, "${it.exception?.message}")
             }
         }
     }
 
     override fun getCurrentJobSeeker(): FirebaseUser? {
-        TODO("Not yet implemented")
+        return auth.currentUser
     }
 
     override fun getJobSeekerById(
         jobSeekerId: String,
         callback: (Boolean, String, JobSeekerModel?) -> Unit
     ) {
-        TODO("Not yet implemented")
+        ref.child(jobSeekerId).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    val jobSeeker = snapshot.getValue(JobSeekerModel::class.java)
+                    if (jobSeeker != null) {
+                        callback(true, "Profile fetched", jobSeeker)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(false, error.message, null)
+            }
+
+        })
     }
 
     override fun getAllJobSeeker(callback: (Boolean, String, List<JobSeekerModel>?) -> Unit) {
-        TODO("Not yet implemented")
+        ref.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    var allJobSeekers = mutableStateListOf<JobSeekerModel>()
+                    for (data in snapshot.children){
+                        var jobSeeker = data.getValue(JobSeekerModel::class.java)
+                        if (jobSeeker != null){
+                            allJobSeekers.add(jobSeeker)
+                        }
+                    }
+                    callback(true, "JobSeeker Fetched", allJobSeekers)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                callback(false,error.message,emptyList())
+            }
+
+        })
     }
 
     override fun logout(
         jobSeekerId: String,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        try {
+            auth.signOut()
+            callback(true, "Logout successfully")
+        } catch (e: Exception) {
+            callback(false, e.message.toString())
+        }
     }
 
     override fun forgetPassword(
         email: String,
         callback: (Boolean, String) -> Unit
     ) {
-        TODO("Not yet implemented")
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
+                callback(true, "Link sent to $email")
+            } else {
+                callback(false, "${it.exception?.message}")
+            }
+        }
     }
 }
