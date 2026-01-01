b696d94f9cafcf80bed0acfe5e6a105c73f0cc41
Labish04 <labishparajuli04@gmail.com>
2026-01-01 19:47:49 +0545

    Dependencies added of zego

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,8 @@ android {
 }
 
 dependencies {
+    implementation("im.zego:zim:2.14.0")
+
     implementation("com.cloudinary:cloudinary-android:2.1.0")
     implementation("io.coil-kt.coil3:coil-compose:3.3.0")
 
